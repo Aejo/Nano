afc667689b9012d17015d37490ac92d77b62f184
Aejo <alejandrocuellom@gmail.com>
2020-06-15 11:40:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.Barras').addEventListener('click',() =>  
+    {
+        document.querySelector('.BottomsPages').classList.toggle('show');
+    }
+);
+
+
+ScrollReveal().reveal('.Games', {delay: 300});
+ScrollReveal().reveal('.Texto-General', {delay: 300});
+
+
